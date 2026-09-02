f6ee44a513c6b47a65986b0c78d59fdec884238f
fakihfakih04-max <fakihfakih04@gmail.com>
2026-09-02 14:56:48 +0300

    Format settings.gradle.kts for better readability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,18 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
-dependencyResolutionManagement { repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS); repositories { google(); mavenCentral() } }
-rootProject.name="DeliveryScanner"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DeliveryScanner"
 include(":app")
