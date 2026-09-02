bb734762085d0c448d081ab7647115db13858d69
fakihfakih04-max <fakihfakih04@gmail.com>
2026-09-02 15:01:30 +0300

    Update compile and kotlin options to Java 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.sff.deliveryscanner"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.sff.deliveryscanner"
         minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
